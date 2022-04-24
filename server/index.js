05d3014a3fcaaf744af9e6a753f1fd05c176563d
Ayushman <Asus@DESKTOP-3RK8C8O.(none)>
2022-04-24 18:21:08 +0530

    Finished! Ready To Deploy!

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/',(req,res)=>{
     res.send("Hello to Memories API!");
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000 ;
 
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true , useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
